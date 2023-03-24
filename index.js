1c6bd9277bf112c46785ba4a4034f164aeccfcfa
Ilayda Guzel <ilaydanurguzel1@gmail.com>
2023-03-24 17:17:05 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin(record){
+    let superWin=record.find(record=>record.result==="W")
+        if(superWin){
+            return superWin.year
+        }
+        else{
+            return undefined
+        }
+};
+
+
